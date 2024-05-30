5724d04821ae351935af0763d4fc3556bde4cd8d
Marcio <marciocere984@gmail.com>
2024-05-30 16:59:06 -0300

    feature/main: structure initial

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Header } from './components/ui/header'
+
+import './styles/global.css'
+
+export function App() {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
---
+++ src/components/ui/header/index.jsx
@@ -0,0 +1,5 @@
+import styles from './style.module.css'
+
+export function Header() {
+  return <header className={styles.header}>Ignite</header>
+} 
