189baf5c0fc4bb80213eafbb75b065d01d8f5cdf
Marcio <marciocere984@gmail.com>
2024-05-30 18:07:49 -0300

    feature/main: adding Header component

--- src/components/ui/header/index.jsx
+++ src/components/ui/header/index.jsx
@@ -1,5 +1,6 @@
 import styles from './style.module.css'
+import Logo from '../../../assets/logo.svg'
 
 export function Header() {
-  return <header className={styles.header}>Ignite</header>
+  return <header className={styles.header}><img alt='logo marca, projeto com react' src={Logo} /></header>
 } 
